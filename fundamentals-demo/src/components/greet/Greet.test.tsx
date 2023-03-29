c27312c1ad4f40bebfdd6da6f8067f0d070b79e3
adam ch <adamanjum83@gmail.com>
2023-03-29 08:15:07 +0500

    folked and setup for learning react unit testing

--- fundamentals-demo/src/components/greet/Greet.test.tsx
+++ fundamentals-demo/src/components/greet/Greet.test.tsx
@@ -1,16 +1,25 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component
+ * it should render hello fellowed by namw
+ */
+
 import { render, screen } from '@testing-library/react'
-import { Greet } from './Greet'
+import Greet from '.'
 
 describe('Greet', () => {
-  test('renders correctly', () => {
+  test('Greet renders correctly', () => {
     render(<Greet />)
-    const textElement = screen.getByText('Hello Guest')
+    const textElement = screen.getByText('Hello')
+
     expect(textElement).toBeInTheDocument()
   })
 
-  test('renders a name', () => {
-    render(<Greet name="Vishwas" />)
-    const textElement = screen.getByText('Hello Vishwas')
-    expect(textElement).toBeInTheDocument()
+  describe('Nested', () => {
+    test('Greet renders with a name', () => {
+      render(<Greet name="Adam" />)
+      const textElement = screen.getByText('Hello Adam')
+
+      expect(textElement).toBeInTheDocument()
+    })
   })
 })
